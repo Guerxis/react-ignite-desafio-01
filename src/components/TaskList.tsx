5fd783506616b4d0cdb33063f1ebec1eeb139220
Marcelo Aguiar <marcelo.aguiar@2scontactcenter.com.br>
2022-05-27 11:03:14 -0300

    conclui metodos do desafio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,31 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    const newTasks = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    };
+
+    setTasks(oldState => [...oldState, newTasks]);
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const taskList = tasks.map(task => task.id == id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks(taskList);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const tasksList = tasks.filter(t => t.id !== id);
+    setTasks(tasksList); 
   }
 
   return (
